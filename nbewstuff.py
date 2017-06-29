3320fc5e923ad0371c435e4d2c1ac15d5f944299
SummerTech <SummerTech@10.144.4.106>
2017-06-29 11:13:54 -0400

    df

--- nbewstuff.py
+++ nbewstuff.py
@@ -1,20 +1,26 @@
-choice = eval(input("how big do you want your triangle?  Please enter the lowest number of stars for the first line."))
-while(True):
-    for squareb in range(0,1):
+'''choice = eval(input("how big do you want your triangle?  Please enter the lowest number of stars for the first line."))
+for squareb in range(0,1):
         for square in range(0,choice):
             print("*",end="")
         print("")
-    for squareb in range(0,1):
+for squareb in range(0,1):
         for square in range(0,choice + 1):
             print("*",end="")
         print("")
-    for squareb in range(0,1):
+for squareb in range(0,1):
         for square in range(0,choice + 2):
             print("*",end="")
         print("")
-    for squareb in range(0,1):
+for squareb in range(0,1):
         for square in range(0,choice + 3):
             print("*",end="")
-        print("")
-   if(choice == number of lines):
-        break
+        print("")'''
+
+for squareb in range(1, 2):
+    print("* ",end="")
+    for square in range(1, 3):
+        print("* ")
+
+
+        
+    
