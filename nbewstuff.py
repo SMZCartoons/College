03da1bbdd46ada56ea74e21335743652cf31c8e0
SummerTech <SummerTech@10.144.4.106>
2017-06-29 10:00:21 -0400

    qqw

---
+++ nbewstuff.py
@@ -0,0 +1,20 @@
+choice = eval(input("how big do you want your triangle?  Please enter the lowest number of stars for the first line."))
+while(True):
+    for squareb in range(0,1):
+        for square in range(0,choice):
+            print("*",end="")
+        print("")
+    for squareb in range(0,1):
+        for square in range(0,choice + 1):
+            print("*",end="")
+        print("")
+    for squareb in range(0,1):
+        for square in range(0,choice + 2):
+            print("*",end="")
+        print("")
+    for squareb in range(0,1):
+        for square in range(0,choice + 3):
+            print("*",end="")
+        print("")
+   if(choice == number of lines):
+        break
--- squareprogram.py
+++ squareprogram.py
@@ -3,8 +3,9 @@ for square in range(0,5):
     for square in range(0,5):
         print("*")
 '''
-     
-for square in range(0,5):
-    for square in range(0,5):
+
+choice = eval(input("how big do you want the square?"))
+for square in range(0,choice):
+    for square in range(0,choice):
         print("* ",end="")
     print("")
