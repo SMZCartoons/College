e911e481891461b7e41a9c68f325a38497a88415
SummerTech <SummerTech@10.144.4.106>
2017-06-30 11:19:09 -0400

    sa

--- tictactoenew.py
+++ tictactoenew.py
@@ -6,17 +6,17 @@ player2 =  "O or X "
     for innerloop in range(0,3):
         print("___ ",end="")'''
 pieces = [["___","___","___"],["___","___","___"],["___","___","___"]]
-print((pieces[0][0]),end="")
-print((pieces[0][1]),end="")
-print((pieces[0][2]),end="")
+print((pieces[0][0]),end=" ")
+print((pieces[0][1]),end=" ")
+print((pieces[0][2]),end=" ")
 print()
-print((pieces[1][0]),end="")
-print((pieces[1][1]),end="")
-print((pieces[1][2]),end="")
+print((pieces[1][0]),end=" ")
+print((pieces[1][1]),end=" ")
+print((pieces[1][2]),end=" ")
 print()
-print((pieces[2][0]),end="")
-print((pieces[2][1]),end="")
-print((pieces[2][2]),end="")
+print((pieces[2][0]),end=" ")
+print((pieces[2][1]),end=" ")
+print((pieces[2][2]),end=" ")
 print()
 piece_chosen = input("Player 1: X or O?")
 if(piece_chosen == "X"):
@@ -25,6 +25,10 @@ if(piece_chosen == "X"):
 if(piece_chosen == "O"):
     print("OK.  You are controlling pieces O")
     print("Player 2: you are controlling pieces X")
-first_move = input("Player 1: Where would you like to move first?")
-
-
+first_move_row = input("Player 1: Which row would you like to move to?")
+first_move_collumn = input("Player 1: Which Column would you like to move to?")
+#if(first_move_row == "0"):
+#    if(first_move_collumn == "0"):
+pieces[eval(first_move_row)][eval(first_move_collumn)] = "X"
+    
+    
