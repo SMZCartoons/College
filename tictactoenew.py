9b986f7c237b82b437f8b1616fd7878239e723f8
SummerTech <SummerTech@10.144.4.106>
2017-06-30 09:03:26 -0400

    nboi

--- tictactoenew.py
+++ tictactoenew.py
@@ -1,6 +1,9 @@
-pieces = [["x","o","x"],["o","x","o"],["x","o","x"]]
-for outerloop in range(0,3):
+#pieces = [["x","o","x"],["o","x","o"],["x","o","x"]]
+'''for outerloop in range(0,3):
     print("")
     for innerloop in range(0,3):
-        print("___ ",end="")
-    print(pieces[outerloop])
+        print("___ ",end="")'''
+pieces = [["x","o","x"],["o","x","o"],["x","o","x"]]
+print(pieces[0])
+print(pieces[1])
+print(pieces[2])
