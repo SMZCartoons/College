fc45b37250f15f8aa1839673bebff63b3e38ab7c
SummerTech <SummerTech@10.144.4.94>
2017-07-25 14:28:46 -0400

    cvfvbnb

--- tictactoenew.py
+++ tictactoenew.py
@@ -18,6 +18,10 @@ print((pieces[2][0]),end=" ")
 print((pieces[2][1]),end=" ")
 print((pieces[2][2]),end=" ")
 print()
+print("INSTRUCTIONS:") 
+print("This is a game of Tic-tac-Toe.  here you must enter your coordinates for where your pieces are.")  
+print("The first row is row 0.  Second row is row 1, and so on.  For the column, it is column 0, 1, and 2.")  
+print("So for instance, if I were to enter the first row and first column, I would say Row 0, Column 0")
 piece_chosen = input("Player 1: X or O?")
 if(piece_chosen == "X"):
     print("OK.  You are controlling pieces X")
@@ -46,6 +50,30 @@ while(True):
     print((pieces[2][1]),end=" ")
     print((pieces[2][2]),end=" ")
     print()
+    if(pieces[0][0] == "X" and pieces[0][1] == "X" and pieces[0][2] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[1][0] == "X" and pieces[1][1] == "X" and pieces[1][2] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[2][0] == "X" and pieces[2][1] == "X" and pieces[2][2] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[0][0] == "X" and pieces[1][1] == "X" and pieces[2][2] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[0][2] == "X" and pieces[1][1] == "X" and pieces[3][0] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[0][0] == "X" and pieces[1][0] == "X" and pieces[2][0] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[0][1] == "X" and pieces[1][1] == "X" and pieces[2][1] == "X"):
+        print("Game Over!")
+        break
+    if(pieces[0][2] == "X" and pieces[1][2] == "X" and pieces[2][2] == "X"):
+        print("Game Over!")
+        break
     
     first_move_row = input("Player 2: Which row would you like to move to?")
     first_move_collumn = input("Player 2: Which column would you like to move to?")
@@ -62,20 +90,30 @@ while(True):
     print((pieces[2][0]),end=" ")
     print((pieces[2][1]),end=" ")
     print((pieces[2][2]),end=" ")
-    if(pieces[0][0] == "X" and pieces[0][1] == "X" and pieces[0][2] == "X"):
-        print("Game Over!")
     if(pieces[0][0] == "O" and pieces[0][1] == "O" and pieces[0][2] == "O"):
         print("Game Over!")
-    if(pieces[1][0] == "X" and pieces[1][1] == "X" and pieces[1][2] == "X"):
-        print("Game Over!")
+        break
     if(pieces[1][0] == "O" and pieces[1][1] == "O" and pieces[1][2] == "O"):
         print("Game Over!")
-    if(pieces[2][0] == "X" and pieces[2][1] == "X" and pieces[2][2] == "X"):
-        print("Game Over!")
+        break
     if(pieces[2][0] == "O" and pieces[2][1] == "O" and pieces[2][2] == "O"):
         print("Game Over!")
-    
-        
+        break
+    if(pieces[0][0] == "O" and pieces[1][1] == "O" and pieces[2][2] == "O"):
+        print("Game Over!")
+        break
+    if(pieces[0][2] == "O" and pieces[1][1] == "O" and pieces[3][0] == "O"):
+        print("Game Over!")
+        break
+    if(pieces[0][0] == "O" and pieces[1][0] == "O" and pieces[2][0] == "O"):
+        print("Game Over!")
+        break
+    if(pieces[0][1] == "O" and pieces[1][1] == "O" and pieces[2][1] == "O"):
+        print("Game Over!")
+        break   
+    if(pieces[0][2] == "O" and pieces[1][2] == "O" and pieces[2][2] == "O"):
+        print("Game Over!")
+        break
      
         
     
