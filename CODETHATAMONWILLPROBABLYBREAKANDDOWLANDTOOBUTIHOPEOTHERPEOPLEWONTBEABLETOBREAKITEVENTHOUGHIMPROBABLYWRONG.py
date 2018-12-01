2747e8f9d505310c2892a5822b12ac22ca931a58
coditum <coditum@10.144.4.40>
2018-12-01 13:44:07 -0500

    dfggdfdd

--- CODETHATAMONWILLPROBABLYBREAKANDDOWLANDTOOBUTIHOPEOTHERPEOPLEWONTBEABLETOBREAKITEVENTHOUGHIMPROBABLYWRONG.py
+++ CODETHATAMONWILLPROBABLYBREAKANDDOWLANDTOOBUTIHOPEOTHERPEOPLEWONTBEABLETOBREAKITEVENTHOUGHIMPROBABLYWRONG.py
@@ -1,7 +1,7 @@
 Numbers1 = [13, 26, 14, 25, 15, 24, 16, 23, 17, 22, 18, 21, 19, 20, 13, 1, 11, 2, 10, 3, 9, 4, 8, 5, 6, 7]
 Combined_Letters = ["ML", "LK", "KJ", "JI", "IH", "HG", "GF", "FE", "ED", "DC", "CB", "BA", "AN", "NO", "OP", "PQ", "QR", "RS", "ST", "TU","UV","VW", "WX", "XY", "YZ", "ZA"]
 Numbers2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52] and ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W" "X", "Y", "Z", " "]
+Alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W" "X", "Y", "Z", " ", "'"]
 
 def unbreakable (input):
     return Numbers1[Alphabet.index(input)]
---
+++ oops.py
@@ -0,0 +1,3 @@
+cards = ("Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King")
+class card:
+    
