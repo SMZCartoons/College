23626f1192a98d9fc0e18414aef4e5e578a22d11
SMZCartoons <45153041+SMZCartoons@users.noreply.github.com>
2020-09-07 14:09:43 -0400

    Delete ex1.py

--- ex1.py
+++
@@ -1,9 +0,0 @@
-choice = eval(input("enter a number"))
-if(choice == 1):
-    print("first if statement is true")
-    if(choice == 2):
-        print("anything")
-elif(choice == 3):
-    print("2nd if statement is true")
-else:
-    print("neither are true")
